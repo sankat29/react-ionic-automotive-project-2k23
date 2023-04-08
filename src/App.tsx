5983c3892ca9e350aec5a5cb69f9ef22c1a8cfe4
SIDDHARTHA <103236725+sankat29@users.noreply.github.com>
2023-04-08 07:55:57 +0530

    wip(app): project init

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { IonApp, IonContent, setupIonicReact } from "@ionic/react";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+/* Theme variables */
+import "./theme/variables.css";
+
+setupIonicReact();
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonContent>
+      <h1>Hello World</h1>
+    </IonContent>
+  </IonApp>
+);
+
+export default App;
